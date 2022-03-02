1ce1f5d746f665f1e9df5a6dc2c4e20daee4868b
Syed-Nawas-Ahamed <syednawasahamed@gmail.com>
2022-03-02 16:49:32 +0530

    App-Added-with-Employee-Model

---
+++ ConsoleAppforGIT/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppforGIT.Models
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
